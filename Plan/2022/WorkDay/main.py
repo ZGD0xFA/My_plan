7e889e8f4d31828ef997fa39bfee5222baaccb56
ZGD0xFA <827621881@qq.com>
2022-02-03 15:18:26 +0800

    add for 2022 year plan
    add for 2022 year plan

---
+++ Plan/2022/WorkDay/main.py
@@ -0,0 +1,45 @@
+# general document for work day
+import shutil
+import os
+
+class WorktoDesk:
+    def __init__(self) -> None:
+        self.path = os.getcwd()
+
+    def genera_dir(self, path):
+        for i in range(0,4):
+            dirweek = os.path.join(path, """{} week""".format(i))
+            
+            for j in ["Plan", "Data", "Done"]:
+                dirname = os.path.join(dirweek, j)
+                try:
+                    os.makedirs(dirname)
+                except:
+                    pass
+            os.chdir(dirweek)
+            fd = open("ReadMe.txt", "w")
+            fd.close()
+        os.chdir(path)
+    
+    def work(self):
+        import calendar
+        for i in calendar.month_name:
+            if not i:
+                continue
+            path = os.path.join(self.path, i)
+            try:
+                os.mkdir(path)
+            except:
+                pass
+
+            if not os.path.isdir(path):
+                continue
+            
+            self.genera_dir(path)
+
+if __name__ == "__main__":
+    work = WorktoDesk()
+    work.work()
+
+    
+        
